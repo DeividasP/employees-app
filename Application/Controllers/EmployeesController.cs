0c6d824002d07cc1dff9db1cdb312be5da29166f
DeividasP <deividas.popelskis@gmail.com>
2017-02-20 18:13:23 +0200

    Employee model, controller and start of index view

---
+++ Application/Controllers/EmployeesController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace Application.Controllers
+{
+
+    public class EmployeesController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+
+}
---
+++ Application/Models/Employee.cs
@@ -0,0 +1,33 @@
+﻿namespace Application.Models
+{
+
+    public class Employee
+    {
+
+        public int ID
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Surname
+        {
+            get;
+            set;
+        }
+
+        public decimal Salary
+        {
+            get;
+            set;
+        }
+
+    }
+
+}
