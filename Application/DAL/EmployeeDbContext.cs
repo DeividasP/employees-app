56d60b13607da0d9c122a948c48853440aedff65
DeividasP <deividas.popelskis@gmail.com>
2017-02-20 19:34:38 +0200

    Employee database context and initializer

---
+++ Application/DAL/EmployeeDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Application.Models;
+using System.Data.Entity;
+
+namespace Application.DAL
+{
+
+    public class EmployeeDbContext : DbContext
+    {
+
+        public EmployeeDbContext() : base("EmployeeDbContext")
+        {
+        }
+
+        public DbSet<Employee> Employees
+        {
+            get;
+            set;
+        }
+
+    }
+
+}
---
+++ Application/DAL/EmployeeDbInitializer.cs
@@ -0,0 +1,26 @@
+﻿using Application.Models;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace Application.DAL
+{
+
+    public class EmployeeDbInitializer : DropCreateDatabaseAlways<EmployeeDbContext>
+    {
+
+        protected override void Seed(EmployeeDbContext context)
+        {
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee { Name = "Deividas", Surname = "Popelskis", Salary = 1000.0m },
+                new Employee { Name = "Augustinas", Surname = "Radauskas", Salary = 500.5m },
+                new Employee { Name = "Karolis", Surname = "Siaurusaitis", Salary = 700.8m },
+            };
+
+            employees.ForEach(e => context.Employees.Add(e));
+            context.SaveChanges();
+        }
+
+    }
+
+}
